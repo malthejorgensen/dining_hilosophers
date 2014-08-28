58388ff634860eb7b16dd5ae637050b520f2dc07
Malthe Jorgensen <malthe.jorgensen@gmail.com>
2014-08-28 16:15:41 +0200

    Fix program exiting before all threads are done
    Haskell doesn’t wait for child threads to finish before terminating
    the main thread, this has to be implemented manually.
    See: http://hackage.haskell.org/package/base-4.7.0.1/docs/Control-Concurrent.html#g:12

--- philosophers.hs
+++ philosophers.hs
@@ -3,19 +3,30 @@ import Control.Concurrent
 import Control.Concurrent.STM
 
 main = do
+  pHasEaten1 <- newEmptyMVar
+  pHasEaten2 <- newEmptyMVar
+  pHasEaten3 <- newEmptyMVar
+  pHasEaten4 <- newEmptyMVar
+  everybodyHasEaten <- newEmptyMVar
+
   fork1  <- newTVarIO False
   knife1 <- newTVarIO False
   fork2  <- newTVarIO False
   knife2 <- newTVarIO False
 
-  forkIO (philosopher 1 fork1 knife1)
-  forkIO (philosopher 2 knife1 fork2)
-  forkIO (philosopher 3 fork2 knife2)
-  forkIO (philosopher 4 knife2 fork1)
+  forkIO (philosopher 1 fork1 knife1 pHasEaten1)
+  forkIO (philosopher 2 knife1 fork2 pHasEaten2)
+  forkIO (philosopher 3 fork2 knife2 pHasEaten3)
+  forkIO (philosopher 4 knife2 fork1 pHasEaten4)
+
+  p1 <- takeMVar pHasEaten1
+  p2 <- takeMVar pHasEaten2
+  p3 <- takeMVar pHasEaten3
+  p4 <- takeMVar pHasEaten4
 
   putStrLn "All done :)"
 
-philosopher i a b = do
+philosopher i a b c = do
   atomically $ do
     a' <- readTVar a
     b' <- readTVar b
@@ -30,3 +41,5 @@ philosopher i a b = do
   atomically $ do
     writeTVar a False
     writeTVar b False
+
+  putMVar c True
